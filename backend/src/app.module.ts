b8a65b9c9afd9f4b25e4447c1605afbf79734fcb
abdelrhman mahmoud shibl <abdulrhman.mahmoud44@gmail.com>
2024-05-07 02:06:38 +0300

    initial commit for backend

---
+++ backend/src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TodoModule } from './todo/todo.module';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    TodoModule,
+    MongooseModule.forRoot(process.env.DB_URI),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
