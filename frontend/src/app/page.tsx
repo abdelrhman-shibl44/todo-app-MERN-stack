a30f81ee0ed66ebbe3c51d040bfa5cecfd4fb32d
abdelrhman mahmoud shibl <abdulrhman.mahmoud44@gmail.com>
2024-05-09 13:55:26 +0300

    initial commit for front-end

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <section className="min-h-[calc(100vh-var(--nav-h))] items-center justify-center relative">
+      Hello world
+    </section>
+  );
+}
---
+++ frontend/tailwind.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+    },
+    extend: {
+      fontSize: {
+        head_two: "clamp(1rem, 5vw, 2rem)",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
