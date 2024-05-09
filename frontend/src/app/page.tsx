640155e809b135d13c8b0a952bdf7089b64480f8
abdelrhman mahmoud shibl <abdulrhman.mahmoud44@gmail.com>
2024-05-09 16:37:39 +0300

    add background to home page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,18 @@
+import Image from "next/image";
 export default function Home() {
   return (
     <section className="min-h-[calc(100vh-var(--nav-h))] items-center justify-center relative">
-      Hello world
+      <div className="absolute inset-0 w-full h-full">
+        <Image
+          style={{ objectFit: "cover" }}
+          src={"/todo-back.webp"}
+          fill
+          alt="todo-background"
+          priority={true}
+          loading="eager"
+        />
+      </div>
+      <div className="backdrop-brightness-90 backdrop-blur-sm absolute inset-0 w-full h-full overflow-y-auto pb-4"></div>
     </section>
   );
 }
