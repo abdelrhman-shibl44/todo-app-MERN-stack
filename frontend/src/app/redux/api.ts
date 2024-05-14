1b252e8662a573d3d11728e7a2a74bc4d42117e7
abdelrhman mahmoud shibl <abdulrhman.mahmoud44@gmail.com>
2024-05-14 05:17:46 +0300

    Add all apis for CRUD Todos

--- frontend/src/app/redux/api.ts
+++ frontend/src/app/redux/api.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { FormData } from "./types";
+import { TodoItem } from "@/Types.common";
 
 const API = axios.create({ baseURL: "http://localhost:3001" });
 // authentication
@@ -8,3 +9,47 @@ export const loginIn = (formData: FormData) =>
 
 export const signUp = (formData: FormData) =>
   API.post("/auth/signup", formData);
+
+// todos
+export const addTodo = (formData: TodoItem, token: string) =>
+  API.post("/todos", formData, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+export const getTodos = (token: string, cates?: string[], limit?: number) =>
+  API.get("/todos", {
+    params: {
+      category: cates?.join(","),
+      limit: cates && cates.length > 0 ? undefined : limit || 4,
+    },
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+// Get single todo
+export const getTodo = (token: string, todoId: string) =>
+  API.get(`/todos/${todoId}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+export const deleteTodos = (token: string, todoId: string) =>
+  API.delete(`/todos/${todoId}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+export const updateTodos = (
+  formData: TodoItem,
+  token: string,
+  todoId: string
+) =>
+  API.put(`/todos/${todoId}`, formData, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
