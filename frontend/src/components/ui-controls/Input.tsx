279aec73f036e8f185fdade9e9017198647edfb0
abdelrhman mahmoud shibl <abdulrhman.mahmoud44@gmail.com>
2024-05-14 04:41:19 +0300

    make types optional

--- frontend/src/components/ui-controls/Input.tsx
+++ frontend/src/components/ui-controls/Input.tsx
@@ -2,12 +2,14 @@ import React from "react";
 type InputProps = {
   className?: string;
   type?: string;
-  name: string;
-  id: string;
-  placeholder: string;
+  name?: string;
+  id?: string;
+  placeholder?: string;
   onChange: React.ChangeEventHandler<HTMLInputElement>;
-  value: string;
+  inputRef?: React.LegacyRef<HTMLInputElement>;
+  value?: string;
   disable?: boolean;
+  checked?: boolean;
 };
 const Input = (props: InputProps) => {
   return (
@@ -17,12 +19,14 @@ const Input = (props: InputProps) => {
           ? props.className
           : "p-2 rounded-md dark:bg-slate-800 w-full outline-none focus:border-t-2 border-orange-500 dark:text-white"
       }
-      type={props.type}
-      name={props.name}
-      id={props.id}
-      placeholder={props.placeholder}
-      onChange={props.onChange}
-      value={props.value}
+      ref={props?.inputRef}
+      type={props?.type}
+      name={props?.name}
+      id={props?.id}
+      placeholder={props?.placeholder}
+      onChange={props?.onChange}
+      value={props?.value}
+      checked={props?.checked}
       disabled={props.disable}
     />
   );
