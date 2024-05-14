30e624d21a564a1334ae513ba780434783b079ef
abdelrhman mahmoud shibl <abdulrhman.mahmoud44@gmail.com>
2024-05-14 06:22:46 +0300

    Create update page

---
+++ frontend/src/app/auth/[id]/update/page.tsx
@@ -0,0 +1,12 @@
+import UpdateForm from "../../../../components/Auth/UpdateForm";
+import Card from "../../../../components/Auth/Card";
+
+function Update() {
+  return (
+    <Card title="Update">
+      <UpdateForm />
+    </Card>
+  );
+}
+
+export default Update;
