ff5379053b98c208844edf92b8f414aeeb2ff6be
abdelrhman mahmoud shibl <abdulrhman.mahmoud44@gmail.com>
2024-05-14 05:15:20 +0300

    Add tailwind-scrollbar plugin to tailwind config file

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -18,6 +18,10 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require("tailwind-scrollbar")({
+      nocompatible: false,
+    }),
+  ],
 };
 export default config;
