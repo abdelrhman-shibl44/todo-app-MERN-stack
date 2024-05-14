54752c1874266e32b58c5146a196c13bb4122dcb
abdelrhman mahmoud shibl <abdulrhman.mahmoud44@gmail.com>
2024-05-14 06:17:11 +0300

    Create Todo Head component

---
+++ frontend/src/components/Todo/TodoHead.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const TodoHead = () => {
+  return (
+    <tr>
+      <th scope="col" className="px-6 py-3">
+        id
+      </th>
+      <th scope="col" className="px-6 py-3">
+        Title
+      </th>
+      <th scope="col" className="px-6 py-3">
+        Description
+      </th>
+      <th scope="col" className="px-6 py-3">
+        Category
+      </th>
+      <th scope="col" className="px-6 py-3">
+        Status
+      </th>
+      <th scope="col" className="px-6 py-3">
+        Action
+      </th>
+    </tr>
+  );
+};
+
+export default TodoHead;
